e155b1dbf343ab638c1928d5de10c993fbd6683d
segfault0x61 <torresj1997@outlook.com>
2024-09-22 09:54:26 -0500

    feat: 🎸 colors utilities

---
+++ src/utils/Colors.ts
@@ -0,0 +1,25 @@
+const Green: number = 0x00ff00;
+const Red: number = 0xff0000;
+const Purple: number = 0x8e44ad;
+const Blue: number = 0x3498db;
+const Yellow: number = 0x3498db;
+const White: number = 0x3498db;
+const Silver: number = 0x99a3a4;
+const Gray: number = 0x616a6b;
+const Pink: number = 0xffc0cb;
+
+let RANDOM;
+RANDOM = [Green, Red, Purple, Blue, Yellow, White, Silver, Gray, Pink];
+
+export default {
+  Green,
+  Red,
+  Purple,
+  Blue,
+  Yellow,
+  White,
+  Silver,
+  Gray,
+  Pink,
+  RANDOM,
+};
